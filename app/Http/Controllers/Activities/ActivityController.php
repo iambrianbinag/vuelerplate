4e4d6b6709ee9f17df1675d0bae43cf84d61e55d
iambrianbinag <brianbaenag@gmail.com>
2021-08-30 13:15:44 +0800

    Add activity service

--- app/Http/Controllers/Activities/ActivityController.php
+++ app/Http/Controllers/Activities/ActivityController.php
@@ -4,12 +4,26 @@ namespace App\Http\Controllers\Activities;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Activities\GetActivitiesRequest;
-use App\Models\Activities\Activity;
-use App\Utils\Util;
+use App\Services\Activities\ActivityService;
 use Illuminate\Http\Request;
 
 class ActivityController extends Controller
-{    
+{        
+    /**
+     * @var ActivityService
+     */
+    protected $activityService;
+    
+    /**
+     * ActivityController constructor
+     *
+     * @param ActivityService $activityService
+     */
+    public function __construct(ActivityService $activityService)
+    {
+        $this->activityService = $activityService;
+    }
+
     /**
      * Get all activity log
      *
@@ -18,56 +32,17 @@ class ActivityController extends Controller
      */
     public function index(GetActivitiesRequest $request)
     {
-        $perPage = $request->per_page ?? config('settings.pagination.per_page');
-        $orderBy = $request->order_by;
-        $orderDirection = $request->order_direction ?? config('settings.pagination.order_direction');
-        $search = $request->search;
-        $notPaginated = $request->not_paginated;
-        $chunkDefault = config('settings.chunk.default');
-        $logName = $request->log_name;
-        $subjectId = $request->subject_id;
+        $activityLog = $this->activityService->getActivities(
+            $request->log_name,
+            $request->subject_id,
+            $request->search, 
+            $request->order_by,
+            $request->order_direction,
+            $request->not_paginated,
+            $request->per_page,
+            null
+        );
 
-        $activityLog = Activity::with('subject', 'causer')
-            ->when($logName, function($query, $logName){
-                return $query->where('log_name', $logName);
-            })
-            ->when($subjectId, function($query, $subjectId){
-                return $query->where('subject_id', $subjectId);
-            })
-            ->when($search, function($query, $search){
-                return $query->where(function($query) use ($search){
-                    $query->where('id', 'like', "%$search%")
-                        ->orWhere('log_name', 'like', "%$search%");
-                });
-            })
-            ->when($orderBy, function($query, $orderBy) use ($orderDirection){
-                return $query->orderBy($orderBy, $orderDirection);
-            }, function($query) use ($orderDirection){
-                return $query->orderBy('created_at', $orderDirection);
-            })
-            ->when($notPaginated, function($query) use ($chunkDefault){
-                $activityLogChunked = [];
-                $query->chunk($chunkDefault, function($activities) use (&$activityLogChunked){
-                    foreach($activities as $activity){
-                        array_push($activityLogChunked, $activity);
-                    }
-                });
-
-                return collect($activityLogChunked);
-            }, function($query) use ($perPage){
-                return $query->paginate($perPage);
-            })
-            ->toArray();
-
-        $onlyAttributes = [
-            'id', 'log_name', 'description', 'created_at', 'updated_at', 'changes', 'subject', 'causer'
-        ];
-
-        if($notPaginated){
-           $activityLog = Util::arrayMapWithAllowedKeys($activityLog, $onlyAttributes);
-        } else {
-            $activityLog['data'] = Util::arrayMapWithAllowedKeys($activityLog['data'], $onlyAttributes);
-        }
         
         return response()->json($activityLog);
     }
---
+++ app/Services/Activities/ActivityService.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Services\Activities;
+
+use App\Models\Activities\Activity;
+use App\Utils\Util;
+
+class ActivityService 
+{          
+    /**
+     * @var Activity
+     */
+    protected $activity;
+
+    /**
+     * @var int
+     */
+    public $paginationPerPageDefault;
+    
+    /**
+     * @var string
+     */
+    public $paginationOrderDirectionDefault;
+    
+    /**
+     * @var int
+     */
+    public $queryChunkDefault;
+    
+    /**
+     *  ActivityService constructor
+     */
+    public function __construct(Activity $activity)
+    {
+        $this->activity = $activity;
+
+        $this->paginationPerPageDefault = config('settings.pagination.per_page');
+
+        $this->paginationOrderDirectionDefault = config('settings.pagination.order_direction');
+
+        $this->queryChunkDefault = config('settings.chunk.default');
+    }
+    
+    /**
+     * Get activity log
+     * 
+     * @param string $logName
+     * @param int $subjectId
+     * @param string $search
+     * @param string $orderBy
+     * @param string $orderDirection
+     * @param $notPaginated 
+     * @param int $perPage
+     * @param int $queryChunk
+     *
+     * @return array
+     */
+    public function getActivities(
+        ?string $logName, 
+        ?int $subjectId, 
+        ?string $search,
+        ?string $orderBy, 
+        ?string $orderDirection,
+        ?bool $notPaginated,
+        ?int $perPage,
+        ?int $queryChunk
+    )
+    {
+        $perPage = $perPage ?? $this->paginationPerPageDefault;
+        $orderDirection = $orderDirection ?? $this->paginationOrderDirectionDefault;
+        $queryChunk = $queryChunk ?? $this->queryChunkDefault;
+
+        $activityLog = $this->activity::with('subject', 'causer')
+            ->when($logName, function($query, $logName){
+                return $query->where('log_name', $logName);
+            })
+            ->when($subjectId, function($query, $subjectId){
+                return $query->where('subject_id', $subjectId);
+            })
+            ->when($search, function($query, $search){
+                return $query->where(function($query) use ($search){
+                    $query->where('id', 'like', "%$search%")
+                        ->orWhere('log_name', 'like', "%$search%");
+                });
+            })
+            ->when($orderBy, function($query, $orderBy) use ($orderDirection){
+                return $query->orderBy($orderBy, $orderDirection);
+            }, function($query) use ($orderDirection){
+                return $query->orderBy('created_at', $orderDirection);
+            })
+            ->when($notPaginated, function($query) use ($queryChunk){
+                $activityLogChunked = [];
+                $query->chunk($queryChunk, function($activities) use (&$activityLogChunked){
+                    foreach($activities as $activity){
+                        array_push($activityLogChunked, $activity);
+                    }
+                });
+
+                return collect($activityLogChunked);
+            }, function($query) use ($perPage){
+                return $query->paginate($perPage);
+            })
+            ->toArray();
+
+        $onlyAttributes = [
+            'id', 'log_name', 'description', 'created_at', 'updated_at', 'changes', 'subject', 'causer'
+        ];
+
+        if($notPaginated){
+           $activityLog = Util::arrayMapWithAllowedKeys($activityLog, $onlyAttributes);
+        } else {
+            $activityLog['data'] = Util::arrayMapWithAllowedKeys($activityLog['data'], $onlyAttributes);
+        }
+        
+        return $activityLog;
+    }
+}
