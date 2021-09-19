f205882882f53d6e66a36139c15849fc470aa087
iambrianbinag <brianbaenag@gmail.com>
2021-09-19 12:19:47 +0800

    Add 'base' alias on webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const path = require('path');
 
 /*
  |--------------------------------------------------------------------------
@@ -13,4 +14,11 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .webpackConfig({
+        resolve: {
+            alias: {
+                "base": path.resolve(__dirname, 'resources/js/core/base')
+            }
+        }
+    });
